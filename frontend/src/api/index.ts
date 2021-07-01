a01ff3fbb66d46f36740d49a9b438422a0206e67
moonee <hellomooneekim@gmail.com>
2021-07-02 06:13:31 +0900

    refactor : 타임아웃에서 api 요청 abort 하도록 수정

--- frontend/src/api/index.ts
+++ frontend/src/api/index.ts
@@ -1,12 +1,14 @@
 import { Category } from "@/types";
 import { backURL, SERVER_ERROR, TIMEOUT } from "@/constants";
 
-const request = async (endPoint, option = {}) => {
-  const response = await fetch(backURL + endPoint, option);
-  const data = await response.json();
+const controller = new AbortController();
+
+const request = async (endPoint: string, time: number) => {
   const timer = setTimeout(() => {
-    throw SERVER_ERROR;
-  }, TIMEOUT);
+    controller.abort();
+  }, time);
+  const response = await fetch(backURL + endPoint);
+  const data = await response.json();
   if (!response.ok) {
     throw SERVER_ERROR;
   }
@@ -15,14 +17,14 @@ const request = async (endPoint, option = {}) => {
 };
 
 const api = {
-  getBest: () => request("/best"),
-  getSummaryContents: () => request("/content"),
+  getBest: () => request("/best", TIMEOUT),
+  getSummaryContents: () => request("/content", TIMEOUT),
   getInfiniteContents: async (category: Category, lastKey: number) => {
-    const data = await request(`/content/${category}/${lastKey}`);
+    const data = await request(`/content/${category}/${lastKey}`, TIMEOUT);
     return { ...data, category };
   },
 
-  getDetailContent: (index: string) => request(`/detail/${index}`),
+  getDetailContent: (index: string) => request(`/detail/${index}`, TIMEOUT),
 };
 
 export default api;
