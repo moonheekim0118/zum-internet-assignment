df2ce4e443f04931a4fc3ea4993b5cf9830d742b
moonee <hellomooneekim@gmail.com>
2021-07-06 09:48:04 +0900

    fix : fetch 옵션에 signal 추가

--- frontend/src/api/index.ts
+++ frontend/src/api/index.ts
@@ -2,12 +2,13 @@ import { Category } from "@/types";
 import { backURL, SERVER_ERROR, TIMEOUT } from "@/constants";
 
 const controller = new AbortController();
+const signal = controller.signal;
 
 const request = async (endPoint: string, time: number) => {
   const timer = setTimeout(() => {
     controller.abort();
   }, time);
-  const response = await fetch(backURL + endPoint);
+  const response = await fetch(backURL + endPoint, { signal });
   const data = await response.json();
   if (!response.ok) {
     throw SERVER_ERROR;
